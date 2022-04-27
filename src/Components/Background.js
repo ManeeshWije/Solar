1558f08ae323ec53d2723dd7ae572c25919e8987
ManeeshWije <m.mwije1@gmail.com>
2022-04-27 14:55:09 -0400

    need to test out textures

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -4,6 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
 
 // Setup
+THREE.ImageUtils.crossOrigin = "";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -13,20 +14,11 @@ document.body.appendChild(renderer.domElement);
 camera.position.setZ(30);
 camera.position.setX(-3);
 
-//const controls = new FlyControls(camera, renderer.domElement);
-//controls.movementSpeed = 100;
-//controls.rollSpeed = Math.PI / 24;
-//controls.autoForward = true;
-//controls.dragToLook = false;
-
-//renderer.render(scene, camera);
-
-renderer.autoClear = false;
+//renderer.autoClear = false;
 
 // Sun
-const sunGeo = new THREE.SphereGeometry(40, 32, 32);
-const sunMaterial = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const sun = new THREE.Mesh(sunGeo, sunMaterial);
+const sunTexture = new THREE.TextureLoader().load();
+const sun = new THREE.Mesh(new THREE.SphereGeometry(40, 32, 32), new THREE.MeshStandardMaterial({ map: sunTexture }));
 scene.add(sun);
 
 // Lights
@@ -44,109 +36,114 @@ scene.add(lightHelper, gridHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // Gonna use this later for asteriod belt and stuff
-const stars = () => {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
+//const stars = () => {
+//const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+//const [x, y, z] = Array(3)
+//.fill()
+//.map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z);
-  scene.add(star);
-};
-Array(200).fill().forEach(stars);
+//star.position.set(x, y, z);
+//scene.add(star);
+//};
+//Array(200).fill().forEach(stars);
+
+// Background
+//const loader = new THREE.TextureLoader();
+//const background = loader.load("../assets/stars.jpg");
+//scene.background = background;
 
 // Create all planets and add them to the scene
 function createPlanets() {
-  // mercury
-  const mercuryGeo = new THREE.SphereGeometry(2, 32, 32);
-  const mercuryMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const mercury = new THREE.Mesh(mercuryGeo, mercuryMaterial);
-  mercury.position.set(0, 0, 60);
-  scene.add(mercury);
+	// mercury
+	const mercuryGeo = new THREE.SphereGeometry(2, 32, 32);
+	const mercuryMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const mercury = new THREE.Mesh(mercuryGeo, mercuryMaterial);
+	mercury.position.set(0, 0, 60);
+	scene.add(mercury);
 
-  // venus
-  const venusGeo = new THREE.SphereGeometry(4, 32, 32);
-  const venusMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const venus = new THREE.Mesh(venusGeo, venusMaterial);
-  venus.position.set(0, 0, 80);
-  scene.add(venus);
+	// venus
+	const venusGeo = new THREE.SphereGeometry(4, 32, 32);
+	const venusMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const venus = new THREE.Mesh(venusGeo, venusMaterial);
+	venus.position.set(0, 0, 80);
+	scene.add(venus);
 
-  // earth
-  const earthGeo = new THREE.SphereGeometry(4, 32, 32);
-  const earthMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const earth = new THREE.Mesh(earthGeo, earthMaterial);
-  earth.position.set(0, 0, 100);
-  scene.add(earth);
+	// earth
+	const earthGeo = new THREE.SphereGeometry(4, 32, 32);
+	const earthMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const earth = new THREE.Mesh(earthGeo, earthMaterial);
+	earth.position.set(0, 0, 100);
+	scene.add(earth);
 
-  // mars
-  const marsGeo = new THREE.SphereGeometry(3, 32, 32);
-  const marsMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const mars = new THREE.Mesh(marsGeo, marsMaterial);
-  mars.position.set(0, 0, 120);
-  scene.add(mars);
+	// mars
+	const marsGeo = new THREE.SphereGeometry(3, 32, 32);
+	const marsMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const mars = new THREE.Mesh(marsGeo, marsMaterial);
+	mars.position.set(0, 0, 120);
+	scene.add(mars);
 
-  // jupiter
-  const jupiterGeo = new THREE.SphereGeometry(10, 32, 32);
-  const jupiterMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const jupiter = new THREE.Mesh(jupiterGeo, jupiterMaterial);
-  jupiter.position.set(0, 0, 160);
-  scene.add(jupiter);
+	// jupiter
+	const jupiterGeo = new THREE.SphereGeometry(10, 32, 32);
+	const jupiterMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const jupiter = new THREE.Mesh(jupiterGeo, jupiterMaterial);
+	jupiter.position.set(0, 0, 160);
+	scene.add(jupiter);
 
-  // saturn
-  const saturnGeo = new THREE.SphereGeometry(9.5, 32, 32);
-  const saturnMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const saturn = new THREE.Mesh(saturnGeo, saturnMaterial);
-  saturn.position.set(0, 0, 200);
-  scene.add(saturn);
+	// saturn
+	const saturnGeo = new THREE.SphereGeometry(9.5, 32, 32);
+	const saturnMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const saturn = new THREE.Mesh(saturnGeo, saturnMaterial);
+	saturn.position.set(0, 0, 200);
+	scene.add(saturn);
 
-  // uranus
-  const uranusGeo = new THREE.SphereGeometry(6, 32, 32);
-  const uranusMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const uranus = new THREE.Mesh(uranusGeo, uranusMaterial);
-  uranus.position.set(0, 0, 240);
-  scene.add(uranus);
+	// uranus
+	const uranusGeo = new THREE.SphereGeometry(6, 32, 32);
+	const uranusMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const uranus = new THREE.Mesh(uranusGeo, uranusMaterial);
+	uranus.position.set(0, 0, 240);
+	scene.add(uranus);
 
-  // neptune
-  const neptuneGeo = new THREE.SphereGeometry(6, 32, 32);
-  const neptuneMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const neptune = new THREE.Mesh(neptuneGeo, neptuneMaterial);
-  neptune.position.set(0, 0, 280);
-  scene.add(neptune);
+	// neptune
+	const neptuneGeo = new THREE.SphereGeometry(6, 32, 32);
+	const neptuneMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const neptune = new THREE.Mesh(neptuneGeo, neptuneMaterial);
+	neptune.position.set(0, 0, 280);
+	scene.add(neptune);
 
-  // pluto
-  const plutoGeo = new THREE.SphereGeometry(2, 32, 32);
-  const plutoMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const pluto = new THREE.Mesh(plutoGeo, plutoMaterial);
-  pluto.position.set(0, 0, 340);
-  scene.add(pluto);
+	// pluto
+	const plutoGeo = new THREE.SphereGeometry(2, 32, 32);
+	const plutoMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const pluto = new THREE.Mesh(plutoGeo, plutoMaterial);
+	pluto.position.set(0, 0, 340);
+	scene.add(pluto);
 
-  // saturn ring
-  const saturnRingGeo = new THREE.RingGeometry(15, 20, 32);
-  const saturnRingMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-  const saturnRing = new THREE.Mesh(saturnRingGeo, saturnRingMaterial);
-  saturnRing.rotateX(45);
-  saturnRing.position.set(0, 0, 200);
-  scene.add(saturnRing);
+	// saturn ring
+	const saturnRingGeo = new THREE.RingGeometry(15, 20, 32);
+	const saturnRingMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+	const saturnRing = new THREE.Mesh(saturnRingGeo, saturnRingMaterial);
+	saturnRing.rotateX(45);
+	saturnRing.position.set(0, 0, 200);
+	scene.add(saturnRing);
 
-  // uranus ring
-  const uranusRingGeo = new THREE.RingGeometry(10, 8, 32);
-  const uranusRingMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-  const uranusRing = new THREE.Mesh(uranusRingGeo, uranusRingMaterial);
-  uranusRing.position.set(0, 0, 240);
-  scene.add(uranusRing);
+	// uranus ring
+	const uranusRingGeo = new THREE.RingGeometry(10, 8, 32);
+	const uranusRingMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+	const uranusRing = new THREE.Mesh(uranusRingGeo, uranusRingMaterial);
+	uranusRing.position.set(0, 0, 240);
+	scene.add(uranusRing);
 }
 createPlanets();
 
 const animate = () => {
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
 };
 
 function Background() {
-  animate();
+	animate();
 }
 
 export default Background;
